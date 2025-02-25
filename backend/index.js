1f66504490ab79ac7bcfb5e71dd148baac627a7c
AmitPanja-2003 <apanja234@gmail.com>
2025-02-26 00:13:14 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://jobhunt-frontend-kby5.onrender.com"],
+    origin:"*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true, // Allow cookies and authentication headers
   };
