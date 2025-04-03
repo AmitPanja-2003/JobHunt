8ecb162474fad6b9042395f27fdd33f1ed58fff2
AmitPanja-2003 <apanja234@gmail.com>
2025-04-03 11:13:42 +0530

    update

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -7,3 +7,4 @@ export const USER_API_END_POINT="https://job-hunt-backend-54xu.onrender.com/api/
 export const JOB_API_END_POINT="https://job-hunt-backend-54xu.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT="https://job-hunt-backend-54xu.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT="https://job-hunt-backend-54xu.onrender.com/api/v1/company";
+
